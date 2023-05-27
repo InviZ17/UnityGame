684c5529eebbc628aa1b97ff72dd72b8377b8749
ecookie <gladiolud64@gmail.com>
2023-05-27 11:30:39 +0300

    added hp bar

--- Project1/Assets/Scripts/PlayerHealth.cs
+++ Project1/Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public float maxHealth = 10; 
     public int health = 4;
     public float invul =1f;
     private float nextInvul;
@@ -16,12 +18,15 @@ public class PlayerHealth : MonoBehaviour
                 health -=damage;
         }
         }
-        
     }
 
     void FixedUpdate(){
-        if (health<=0){
+        if (health <= 0)
+        {
             Destroy(this.gameObject);
         }
+
+        Slider s = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<Slider>();
+        s.value = health / maxHealth;
     }
 }
